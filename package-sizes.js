09419ca6a7bc044a359436cc38c1df26110dcd2f
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2024-08-03 03:33:08 +0300

    WIP

---
+++ package-sizes.js
@@ -0,0 +1,10 @@
+window.packageSizes = {
+  "kaspa": "9.07 MB",
+  "kaspa-core": "9.07 MB",
+  "kaspa-core-dev": "35.40 MB",
+  "kaspa-dev": "35.40 MB",
+  "kaspa-keygen": "1.68 MB",
+  "kaspa-keygen-dev": "3.32 MB",
+  "kaspa-rpc": "3.08 MB",
+  "kaspa-rpc-dev": "16.98 MB"
+};
